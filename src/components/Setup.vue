b33a11e7759bf7ea13fad2d40bd65efbb282e9e6
Xavier Armijos <xaah@users.noreply.github.com>
2021-07-21 13:25:40 +0100

    copy to clipboard import (#320)

--- src/components/Setup.vue
+++ src/components/Setup.vue
@@ -6,11 +6,21 @@
     <h3 id="login"><a href="#login" aria-hidden="true">#</a>Login</h3>
     <p>Once you install the CLI, make sure you are logged in with:</p>
     <pre class="language-console"><code class="language-console">reliably auth login</code></pre>
-    <button data-v-ba8b7792="" class="ctc-button button button--small">
-        <svg data-v-ba8b7792="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="clipboard_svg__feather clipboard_svg__feather-clipboard"><path d="M16 4h2a2 2 0 012 2v14a2 2 0 01-2 2H6a2 2 0 01-2-2V6a2 2 0 012-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
-  Copy <span data-v-ba8b7792="" class="screen-reader-text">to clipboard</span></button><p> Additional information can be found in the <a href="/docs/getting-started/login/#login">Login</a> section.</p>
+    <CopyToClipboard />
+    <p> Additional information can be found in the <a href="/docs/getting-started/login/#login">Login</a> section.</p>
   </span>
 </template>
+
+<script>
+
+import CopyToClipboard from '~/components/MarkdownCopyToClipboard.vue'
+
+export default {
+    components: {
+        CopyToClipboard
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 .setup {
